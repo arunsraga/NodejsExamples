e64cd5e81a6262ade06f7036e56884e05ad83c97
arunsraga <arunsraga@gmail.com>
2020-12-30 21:30:33 +0530

    CPU Intensive task example

---
+++ cpuIntensive/fibonacci.js
@@ -0,0 +1,14 @@
+
+const fibonacci = (n) => {
+	if(n<=2){
+		return 1
+	} else {
+		return fibonacci(n-1) + fibonacci(n-2)
+	}
+}
+
+
+sTime = process.hrtime()
+console.log(fibonacci(45))
+console.log(process.hrtime(sTime))
+
